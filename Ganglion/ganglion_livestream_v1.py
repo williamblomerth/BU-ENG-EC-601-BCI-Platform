dead3ebe097a0e2ef8c70381c57f83c63d7167fc
William Blomerth <blomerth.w@gmail.com>
2025-11-02 14:43:47 -0500

    data preprocessing

--- Ganglion/ganglion_livestream_v1.py
+++ Ganglion/ganglion_livestream_v1.py
@@ -3,9 +3,17 @@
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 import time
 import sys
+import csv
+import os
+from tkinter import filedialog
+import scipy
+import mne
 
 def main():
     try:
+        dir = filedialog.askdirectory()
+        file_dir = os.path.join(dir, "ganglion_data.csv")
+
         params = BrainFlowInputParams()
         params.serial_port = "COM6"
 
@@ -26,13 +34,30 @@ def main():
         board.start_stream()
 
         while True:
-            time.sleep(5)
-            # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+            time.sleep(1)
             data = board.get_board_data()  # get all data and remove it from internal buffer
-            print(data)
+            eeg_channels = BoardShim.get_eeg_channels(BoardIds.GANGLION_BOARD.value)
 
-    except:
-        print("Stream Interrupt")
+            info = mne.create_info(ch_names=['Fp1','Fp2','TP7','TP8'], sfreq=200, ch_types='eeg')
+            raw = mne.io.RawArray(data[eeg_channels, :], info)
+            raw_resampled = raw.resample(128)
+            write_data = raw_resampled.get_data()  # numpy array: shape (4, N)
+            write_data = write_data.T  # transpose so each row = one time sample, columns = channels
+
+            #convert from uV into mV
+            write_data = write_data / 1000.0
+
+            with open(file_dir, 'w', newline='') as csvfile:
+                spamwriter = csv.writer(csvfile)
+                spamwriter.writerow(['Fp1', 'Fp2', 'TP7', 'TP8'])
+                spamwriter.writerows(write_data)
+
+            print(write_data)
+
+    except Exception as e:
+        print(f"Stream Interrupt: {e}")
+        # traceback.print_exc()
+        # print("Stream Interrupt")
         sys.exit()
 
     finally:
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BU-ENG-EC-601-BCI-Platform
 Project repository for ENG EC 601 at Boston University.
+
+![alt text](image.png)
+
